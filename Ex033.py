8e177fd1f187ecc79cf8110f322a61791c8ea4d2
Genilton-Matoso <geniltonmatoso21@gmail.com>
2025-09-19 22:32:36 -0300

    Add Ex33

---
+++ Ex033.py
@@ -0,0 +1,8 @@
+p1 = int(input('Primeiro valor: '))
+p2 = int(input('Segundo valor: '))
+p3 = int(input('Terceiro valor: '))
+
+lista = [p1, p2, p3]
+
+print(f'O menor valor digitado foi {min(lista)}')
+print(f'O menor valor digitado foi {max(lista)}')
