482b5a2f008b64a27b910bf9fe1167afb65292e2
Genilton-Matoso <geniltonmatoso21@gmail.com>
2025-09-19 22:01:16 -0300

    ADD...

--- Ex028.py
+++ Ex028.py
@@ -1,15 +1,13 @@
 from random import randint
-from time import sleep
 
 numero_computador = randint(0,5)
 
 numero_usuario = int(input('Chute um numero entre 0 e 5: '))
-print('Processando...')
-sleep(2)
+
 if numero_computador == numero_usuario:
-    print('Parabéns... Voce acertou!')
+    print('Voce acertou!')
 
 else:
-    print(f'Voce errou! Eu pensei no número {numero_computador}')
+    print('Voce errou!')
 
 
