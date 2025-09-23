36a174305cf960602e55103b3119ef890e6bfffd
Genilton-Matoso <geniltonmatoso21@gmail.com>
2025-09-22 21:43:11 -0300

    Add Ex34

---
+++ Ex034.py
@@ -0,0 +1,8 @@
+salario = float(input('Qual o salário do funcionário ? R$'))
+
+if salario >= 1250:
+    novo_salario = salario * 1.10
+else:
+    novo_salario = salario * 1.15
+
+print(f'Quem ganhava R${salario:.2f} passa a ganhar R${novo_salario:.2f}')
