54f7782a17d50974b19b10283a6bf7e3121a4503
Genilton-Matoso <geniltonmatoso21@gmail.com>
2025-10-13 22:01:52 -0300

    ADD Ex51 - cc

---
+++ Ex051.py
@@ -0,0 +1,10 @@
+print(30*'=')
+print('10 TERMOS DE UMA PA')
+print(30*'=')
+
+p = int(input('Digite o primeiro termo: '))
+r = int(input('Razão: '))
+decimo = p + (10-1) * r
+for i in range(p,decimo + r,r):
+    print(i, end=' ')
+print('FIM')
