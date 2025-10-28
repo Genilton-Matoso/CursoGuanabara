bf80852d2573ac722a54d4101ec9c656f9ffb72f
Genilton-Matoso <geniltonmatoso21@gmail.com>
2025-10-27 22:17:43 -0300

    ADD Ex075

---
+++ Ex075.py
@@ -0,0 +1,17 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+n3 = int(input('Digite mais um número: '))
+n4 = int(input('Digite o último número: '))
+
+tupla = (n1, n2, n3, n4)
+print(f'Você digitou {tupla}')
+print(f'O valor 9 apareceu {tupla.count(9)} vezes')
+if 3 in tupla:
+    print(f'O valor 3 apareceu na {tupla.index(3)+1}ª posição')
+else:
+    print('O valor 3 não foi digitado')
+par = 0
+for n in tupla:
+    if n % 2 == 0:
+        par += 1
+print(f'Você digitou {par} valores pares')
